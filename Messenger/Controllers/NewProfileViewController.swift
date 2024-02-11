a62d74c03ad4091c12a175f752547f845f9427a7
pratikHan <patrick.hanchate@gmail.com>
2024-02-10 17:32:56 -0700

    Setup Directories

---
+++ Messenger/Controllers/NewProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewProfileViewController.swift
+//  Messenger
+//
+//  Created by Pratik Hanchate on 2/10/24.
+//
+
+import UIKit
+
+class NewProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
